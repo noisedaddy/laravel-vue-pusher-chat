09b6c96d378fd0abf0c848fa1cfd175989029f8e
Veljko <veljko.radenkovic@gmail.com>
2019-01-17 17:57:17 +0100

    initial

---
+++ resources/js/components/ChatMessage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="col-md-8">
+        <div class="card">
+            <div class="card-header">Chat</div>
+            <div class="card-body">
+                Messages
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
